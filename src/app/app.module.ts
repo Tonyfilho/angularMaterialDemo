d8e9ecc82ada9469fa0fb851ce9974986adb8ea5
tony filho <antonio.m.filho>
2021-03-04 15:54:51 +0000

    Material DatePicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,28 @@ import { AppComponent } from './app.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { RadioButtonsComponent } from './radio-buttons/radio-buttons.component';
 import { SelectsComponent } from './selects/selects.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
 
 import {MatCheckboxModule, } from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule, } from '@angular/material/datepicker'
+import {MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CheckboxComponent,
     RadioButtonsComponent,
-    SelectsComponent
+    SelectsComponent,
+    DatePickerComponent,
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -29,8 +38,13 @@ import {MatSelectModule} from '@angular/material/select';
     MatRadioModule,
     MatCheckboxModule,
     MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
 
   ],
+  exports: [MatInputModule, MatFormFieldModule],
   providers: [],
   bootstrap: [AppComponent]
 })
